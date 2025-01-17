e3a259d70c087434bd47952ede33c4d6d7717135
Renato Mariano <131804750+lobobranco96@users.noreply.github.com>
2025-01-16 23:37:56 -0300

    Update extract_one.py

--- mnt/airflow/dags/python/extract_one.py
+++ mnt/airflow/dags/python/extract_one.py
@@ -5,7 +5,7 @@ import json
 HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7",
-        "Referer": "https://www.webmotors.com.br",
+        "Referer": "https://
         "Connection": "keep-alive"
     }
 
